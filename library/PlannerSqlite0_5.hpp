1a737d007830e45fe5f346b49ace16f2cacd6e95
talesm <tales.miranda84@gmail.com>
2016-01-17 17:37:36 -0200

    Started designing the library.

---
+++ library/PlannerSqlite0_5.hpp
@@ -0,0 +1,13 @@
+#ifndef ORCA_PLANNER_SQLITE_0_5_HPP
+#define ORCA_PLANNER_SQLITE_0_5_HPP
+
+namespace orca {
+/**
+ * Implements a Sqlite Planner for the format 0.5
+ */
+class PlannerSqlite0_5 {
+  /**/
+};
+} /* orca */
+
+#endif /* end of include guard: ORCA_PLANNER_SQLITE_0_5_HPP */
---
+++ test/PlannerSqlite0_5.cpp
@@ -0,0 +1,8 @@
+#include <catch.hpp>
+#include "PlannerSqlite0_5.hpp"
+
+using namespace orca;
+
+TEST_CASE("PlannerSqlite0_5", "[controller][planner][model-interface]") {
+  PlannerSqlite0_5 planner{};
+}
