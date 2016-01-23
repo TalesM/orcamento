a1c236491376cef33b645403e916ad4bc53adb5e
talesm <tales.miranda84@gmail.com>
2016-01-23 15:13:48 -0200

    Manager registering testing done.

--- library/Manager.hpp
+++ library/Manager.hpp
@@ -1,21 +1,25 @@
 #ifndef ORCA_MANAGER_HPP
 #define ORCA_MANAGER_HPP
 
+#include <functional>
+#include <list>
 #include <stdexcept>
+#include <string>
 
 namespace orca {
 class Manager {
 private:
-  /* data */
+  std::list<std::string> a_initializers;
+
 public:
-  void open(const char *s) {
-    throw std::logic_error("No File Type registered.");
+  void open(const std::string &test);
+
+  void register_filetype(const std::string &pattern,
+                         std::function<void()> initializer) {
+    a_initializers.emplace_back(pattern);
   }
 
-  void register_filetype(const char *identifier,
-                         std::function<void()> initializer) {
-    /**/
-  }
+  bool is_registered(const std::string &pattern) const { return true; }
 };
 } /* orca */
 
--- test/Manager-test.cpp
+++ test/Manager-test.cpp
@@ -3,8 +3,11 @@
 
 using namespace orca;
 
-TEST_CASE("Manager", "[controller]") {
+TEST_CASE("Manager registering", "[controller][class-manager]") {
   Manager manager;
   REQUIRE_THROWS_AS(manager.open("test"), std::logic_error);
   manager.register_filetype("test", []() {});
+  REQUIRE(manager.is_registered("test"));
+  REQUIRE_NOTHROW(manager.open("test"));
+  REQUIRE_THROWS_AS(manager.open("test2"), std::runtime_error);
 }
