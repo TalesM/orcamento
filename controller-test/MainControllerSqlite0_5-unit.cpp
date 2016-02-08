6563af5774bef8d796466100981404c2167de38e
talesm <tales.miranda84@gmail.com>
2016-02-08 01:26:37 -0200

    Created table budget.
    Created mechanism to avoid typing sql in the midle of the text.

--- controller-test/MainControllerSqlite0_5-unit.cpp
+++ controller-test/MainControllerSqlite0_5-unit.cpp
@@ -8,8 +8,16 @@ TEST_CASE("MainControllerSqlite0_5 new file", "[controller][planner][class-mainc
   MainControllerSqlite0_5 planner{""};
   auto &db = planner.db();
 
-  REQUIRE((int)db.execAndGet("SELECT COUNT(name) FROM sqlite_master WHERE "
-                             "type='table' AND name like 'meta'") == 1);
+  INFO("Number of Tables" << (int)db.execAndGet("SELECT COUNT(name) FROM sqlite_master WHERE "
+                                                "type='table'"))
+  auto checkTable = [&db](const string &name) -> int {
+    return db.execAndGet(
+               "SELECT COUNT(name) FROM sqlite_master WHERE "
+               "type='table' AND name like '" +
+               name + "'");
+  };
+  REQUIRE(checkTable("meta"));
+  REQUIRE(checkTable("budget"));
   std::string format_name = db.execAndGet("SELECT value FROM meta WHERE key = 'format-name'");
   REQUIRE(format_name == "orcamento");
 
@@ -18,11 +26,12 @@ TEST_CASE("MainControllerSqlite0_5 new file", "[controller][planner][class-mainc
   REQUIRE(planner.format() == "OrcaMento file format 0.5.0");
 }
 
-TEST_CASE("MainControllerSqlite0_5 load file",  "[controller][planner][class-maincontrollersqlite0_5][unit]"){
+TEST_CASE("MainControllerSqlite0_5 load file", "[controller][planner][class-maincontrollersqlite0_5][unit]")
+{
   string filename = tmpnam(nullptr);
   {
-    //Creation
-    MainControllerSqlite0_5 planner{filename}; //Saving too.
+    // Creation
+    MainControllerSqlite0_5 planner{filename};  // Saving too.
     auto &db = planner.db();
     db.exec("INSERT INTO meta VALUES('test', 'aTest')");
   }
@@ -35,7 +44,8 @@ TEST_CASE("MainControllerSqlite0_5 load file",  "[controller][planner][class-mai
   REQUIRE(format_name == "aTest");
 }
 
-TEST_CASE("MainControllerSqlite0_5 listBudgets", "[controller][planner][class-maincontrollersqlite0_5][unit]") {
+TEST_CASE("MainControllerSqlite0_5 listBudgets", "[controller][planner][class-maincontrollersqlite0_5][unit]")
+{
   MainControllerSqlite0_5 planner{""};
   auto &db = planner.db();
   std::vector<std::string> vec = planner.listBudgets();
